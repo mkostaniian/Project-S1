2ad2a36d73a582ea1161300024b928a15f31d340
Milena Kostanian <milenakostanian70@gmail.com>
2025-12-09 20:48:06 +0400

    Add files via upload

---
+++ rpn.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h> 
+
+#define MAX_STACK_SIZE 100
+int stack[MAX_STACK_SIZE];
+int top = -1;
+
+void push(int value) {
+    if (top >= MAX_STACK_SIZE - 1) {
+        printf("Stack overflow!\n");
+        exit(1);
+    }
+    stack[++top] = value;
+}
+
+int pop() {
+    if (top < 0) {
+        printf("Stack underflow!\n");
+        exit(1);
+  }
+    return stack[top--];
+}
+
+int main() {
+    char input[256];
+    char *token;
+    int operand1, operand2, result;
+
+    printf("Enter RPN expression (e.g., 3 4 + 2 *): ");
+    fgets(input, sizeof(input), stdin);
+
+    token = strtok(input, " \n"); 
+    while (token != NULL) {
+        if (isdigit(token[0]) || (token[0] == '-' && isdigit(token[1]))) { 
+            push(atoi(token));
+        } else { 
+            operand2 = pop();
+            operand1 = pop();
+
+            switch (*token) {
+                case '+':
+                    result = operand1 + operand2;
+                    break;
+                case '-':
+                    result = operand1 - operand2;
+                    break;
+                case '*':
+                    result = operand1 * operand2;
+                    break;
+                case '/':
+                    if (operand2 == 0) {
+                        printf("Division by zero!\n");
+                        exit(1);
+                    }
+                    result = operand1 / operand2;
+                    break;
+                default:
+                    printf("Invalid operator: %s\n", token);
+                    exit(1);
+            }
+            push(result);
+        }
+        token = strtok(NULL, " \n");
+    }
+
+    if (top == 0) { 
+        printf("Result: %d\n", pop());
+    } else {
+        printf("Invalid RPN expression.\n");
+    }
+
+    return 0;
+}
